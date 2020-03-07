0fc8c6e43b719f27da347ff7b02019b5b207317c
Carsten Dolar <carsten.dolar@gmail.com>
2020-03-07 22:33:24 +0100

    broadcast stdout via socketio

--- xr18_rec/views/api.py
+++ xr18_rec/views/api.py
@@ -1,13 +1,48 @@
 from flask import Blueprint, jsonify, make_response
 from flask import current_app as app
+from flask_socketio import emit
 
-from subprocess import Popen, PIPE, TimeoutExpired
+from subprocess import Popen, PIPE, TimeoutExpired, STDOUT
 from datetime import datetime
 from time import sleep
 
+from xr18_rec import socketio
+
+from threading import Thread
+
+
 import os
 
+
 api = Blueprint("api", __name__)
+global proc, worker
+proc = None
+
+def relayStdOut():
+    global proc
+    while True:
+        if proc is not None:
+            for line in iter(proc.stdout.readline,''):
+                socketio.emit('cmd_line_output',{"stdout":line})
+
+worker = Thread(target=relayStdOut, daemon=True)
+worker.start()
+
+def getStatus():
+    """ Helper Function to get status
+    """
+    status = []
+    global proc
+    if proc is None:
+        status.append("No recording running")
+    else:
+        retcode = proc.poll()
+        if retcode is None:
+            status.append("recording ongoing")
+        else:
+            status.append("recording stopped with returncode {}".format(retcode))
+        return status
+
 
 @api.route("/rec")
 def rec():
@@ -29,15 +64,10 @@ def rec():
         filename
     ]
     global proc
-    proc = Popen(cmd, env=env, stdout=PIPE, stderr=PIPE, text=True)
-    output = ""
-    err = ""
-    #try:
-    #    (output, err) = proc.communicate(timeout=2)
-    #except TimeoutExpired:
-    #    pass
-    
-    return make_response(jsonify({"status":"started recording", "filename":filename, "output":output, "error":err, "pid":proc.pid,"args":proc.args}))
+    proc = Popen(cmd, env=env, stdout=PIPE, stderr=STDOUT, text=True)
+    # get immediate status
+    status = getStatus()
+    return make_response(jsonify({"status":"started recording", "filename":filename, "status":status,"pid":proc.pid,"args":proc.args}))
 
 @api.route("/stop")
 def stop():
@@ -56,14 +86,6 @@ def stop():
 
 @api.route("/status")
 def status():
-    status = ""
-    global proc
-    if proc is None:
-        status = "No recording running"
-    else:
-        retcode = proc.poll()
-        if retcode is None:
-            status = "recording ongoing"
-        else:
-            status = "recording stopped with returncode {}".format(retcode)
+    status = getStatus()
     return make_response(jsonify({"status":status}))
+
