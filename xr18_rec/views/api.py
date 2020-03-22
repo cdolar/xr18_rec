2b6b8215d821046417d85dae53e7d37974a12397
Carsten Dolar <carsten.dolar@gmail.com>
2020-03-22 21:43:33 +0100

    Adding playback and download to API

--- xr18_rec/views/api.py
+++ xr18_rec/views/api.py
@@ -1,17 +1,13 @@
-from flask import Blueprint, jsonify, make_response
-from flask import current_app as app
-from flask_socketio import emit
-
+import os, os.path
 from subprocess import Popen, PIPE, TimeoutExpired, STDOUT
 from datetime import datetime
-from time import sleep
-
-from xr18_rec import socketio
-
+from time import sleep, localtime
+from glob import glob
 from threading import Thread
-
-
-import os
+from flask import Blueprint, jsonify, make_response, request, send_from_directory, abort
+from flask import current_app as app
+from flask_socketio import emit
+from xr18_rec import socketio
 
 
 api = Blueprint("api", __name__)
@@ -28,30 +24,33 @@ def relayStdOut():
 worker = Thread(target=relayStdOut, daemon=True)
 worker.start()
 
+
 def getStatus():
     """ Helper Function to get status
     """
     status = []
     global proc
     if proc is None:
-        status.append("No recording running")
+        status.append("No recording/playback running")
     else:
         retcode = proc.poll()
         if retcode is None:
-            status.append("recording ongoing")
+            status.append("recording/playback ongoing")
         else:
-            status.append("recording stopped with returncode {}".format(retcode))
+            status.append("recording/playback stopped with returncode {}".format(retcode))
         return status
 
 
 @api.route("/rec")
 def rec():
     filename = datetime.now().strftime('%Y-%m-%d--%H-%M-%S__xr18rec.wav')
-    audiodev = "hw:X18XR18,0"
-    #audiodev = "hw:CODEC,0"
+    audiodev = request.args.get("audiodev")
+    if audiodev is None:
+        #audiodev = "hw:X18XR18,0"
+        audiodev = "hw:CODEC,0"
     buffersize = 512*1024
     channels = 18
-    bits = 24
+    bits = 32
     rate = 48000
     env = os.environ
     env['AUDIODEV']=audiodev
@@ -64,10 +63,24 @@ def rec():
         filename
     ]
     global proc
+    if proc is not None:
+        stop()
     proc = Popen(cmd, env=env, stdout=PIPE, stderr=STDOUT, text=True)
     # get immediate status
     status = getStatus()
-    return make_response(jsonify({"status":"started recording", "filename":filename, "status":status,"pid":proc.pid,"args":proc.args}))
+    return make_response(
+        jsonify(
+            {
+                "status":"started recording",
+                "filename":filename,
+                "status":status,
+                "pid":proc.pid,
+                "args":proc.args,
+                "audiodev":audiodev
+            }
+        )
+    )
+
 
 @api.route("/stop")
 def stop():
@@ -78,14 +91,72 @@ def stop():
     else:
         retcode = proc.poll()
         if retcode is None:
-            status = "recording ongoing, terminating it"
+            status = "recording/playback ongoing, terminating it"
             proc.terminate()
         else:
-            status = "recording stopped with returncode {}".format(retcode)
+            status = "recording/playback stopped with returncode {}".format(retcode)
     return make_response(jsonify({"status":status}))
+
 
 @api.route("/status")
 def status():
     status = getStatus()
     return make_response(jsonify({"status":status}))
+
+
+@api.route("/files")
+def get_files():
+    files = sorted(glob("*.wav"))
+    filedata = {"files":[]}
+    for f in files:
+        filedata["files"].append({"filename": f, "creation_time": localtime(os.path.getctime(f))})
+    return make_response(jsonify(filedata))
+
+
+@api.route("/play/<filename>")
+def play(filename):
+    audiodev = request.args.get("audiodev")
+    if audiodev is None:
+        #audiodev = "hw:X18XR18,0"
+        audiodev = "hw:CODEC,0"
+    buffersize = 512*1024
+    channels = 18
+    bits = 24
+    rate = 48000
+    env = os.environ
+    env['AUDIODEV']=audiodev
+    cmd = [
+        'play',
+        '--buffer={}'.format(buffersize),
+        '-c {}'.format(channels),
+        '-b {}'.format(bits),
+        '-r {}'.format(rate),
+        filename
+    ]
+    global proc
+    if proc is not None:
+        stop()
+    proc = Popen(cmd, env=env, stdout=PIPE, stderr=STDOUT, text=True)
+    # get immediate status
+    status = getStatus()
+    return make_response(
+        jsonify(
+            {
+                "status":"started playback", 
+                "filename":filename, 
+                "status":status,
+                "pid":proc.pid,
+                "args":proc.args,
+                "audiodev":audiodev
+            }
+        )
+    )
+
+@api.route("/download/<filename>")
+def download(filename):
+    print(filename)
+    try:
+        return send_from_directory(os.path.abspath("."), filename=filename, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
